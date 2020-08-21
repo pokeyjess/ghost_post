aa0edfec0553a1b3c32ef92b2a478bc72eb7f393
Jessica Benson <pokeyjess72@gmail.com>
2020-08-21 00:59:53 -0400

    Lists posts and time on homepage

--- ghost_post/urls.py
+++ ghost_post/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from ghost_post_app.views import index, post_form_view
 
 urlpatterns = [
-    path('', index),
+    path('', index, name="homepage"),
     path('postform/', post_form_view),
     path('admin/', admin.site.urls),
 ]
--- ghost_post_app/views.py
+++ ghost_post_app/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
 from ghost_post_app.models import Posts
 from ghost_post_app.forms import PostForm
 
 def index(request):
-    return render(request, "index.html", {"hello": "Hello"})
+    post_list = Posts.objects.all().order_by('-post_time')
+    return render(request, "index.html", {"post_list": post_list, "hello": "Hello"})
 
 def post_form_view(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            Posts.objects.create(boast_or_roast=data['choice'], content=data['content'], up_votes=0, down_votes=0)
+            return HttpResponseRedirect(reverse('homepage'))
+        
     form = PostForm()
     return render(request, "post_form.html", {"form": form})
